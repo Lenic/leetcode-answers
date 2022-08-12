a6008a79c19b70a82858f4448b44a841afa49c66
Lenic <Lenic@live.cn>
2022-08-12 19:58:27 +0800

    完成单调栈的 42 题解
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/42.js
+++ src/code/42.js
@@ -3,49 +3,31 @@
  * @return {number}
  */
 const trap = function (height) {
-  if (height.length < 2) return 0;
-
-  const lr = height.reduceRight((stack, x, i) => {
-    if (x === 0) return stack;
-
-    while (stack.length && height[stack[stack.length - 1]] < x) {
-      stack.pop();
-    }
-    stack.push(i);
-
-    return stack;
-  }, []);
-
-  const rl = [];
-  for (let i = lr.length ? lr[0] : 0; i < height.length; i++) {
-    const item = height[i];
-    if (item === 0) continue;
-
-    while (rl.length && height[rl[rl.length - 1]] <= item) {
-      rl.pop();
-    }
-    rl.push(i);
-  }
-
   let res = 0;
-  lr.reduceRight((from, to) => {
-    const currentHeight = Math.min(height[from], height[to]);
-    for (let i = from + 1; i < to; i++) {
-      res += currentHeight - height[i];
-    }
-    return to;
-  });
 
-  rl.reduce((from, to) => {
-    const currentHeight = Math.min(height[from], height[to]);
-    for (let i = from + 1; i < to; i++) {
-      res += currentHeight - height[i];
+  const stack = [];
+  height.forEach((x, i) => {
+    while (stack.length && height[stack[stack.length - 1]] < x) {
+      const topIndex = stack.pop();
+
+      if (!stack.length) break;
+
+      const leftIndex = stack[stack.length - 1];
+      const currentHeight = Math.min(height[leftIndex], height[i]) - height[topIndex];
+
+      res += (i - leftIndex - 1) * currentHeight;
     }
-    return to;
+
+    stack.push(i);
   });
 
   return res;
 };
 
 // 6
-console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+//                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11
+// console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+
+// 9
+//                0, 1, 2, 3, 4, 5
+console.log(trap([4, 2, 0, 3, 2, 5]));
