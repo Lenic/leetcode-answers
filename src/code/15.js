9fd5dce3e72cb48563ccc2f559cbe3c4ffe7b44b
Lenic <Lenic@live.cn>
2022-08-13 23:15:03 +0800

    优化过的三数之和：15
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/15.js
+++ src/code/15.js
@@ -5,23 +5,38 @@
 var threeSum = function (nums) {
   if (nums.length < 3) return [];
 
-  const res = {};
+  nums.sort((x, y) => x - y);
+
+  const res = [];
   const max = nums.length - 2;
   for (let i = 0; i < max; i++) {
-    const restValue = 0 - nums[i];
-    const cache = {};
-    for (let j = i + 1; j < nums.length; j++) {
-      const item = nums[j];
-      if (cache[item] !== void 0) {
-        const list = [nums[i], cache[item], item].sort((x, y) => x - y);
-        res[list.join(',')] = list;
-        continue;
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) {
+          left += 1;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right -= 1;
+        }
+        left += 1;
+        right -= 1;
+      } else if (sum < 0) {
+        left += 1;
+      } else {
+        right -= 1;
       }
-      cache[restValue - item] = item;
     }
   }
-  return Object.keys(res).map((v) => res[v]);
+  return res;
 };
 
 // [[-1,-1,2],[-1,0,1]]
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([-1, 0, 1, 2, -1, -4, -1]));
