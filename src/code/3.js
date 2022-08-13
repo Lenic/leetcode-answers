9ae9d78ea91fac7a179d3c1e44781770061f7742
Lenic <Lenic@live.cn>
2022-08-13 21:47:35 +0800

    优化过的无重复最长子串：3
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/3.js
+++ src/code/3.js
@@ -3,38 +3,31 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  if (!s.length) return '';
-
-  let res = 0;
-
   let leftIndex = 0;
   let rightIndex = 0;
 
   const list = new Set();
 
-  for (; rightIndex < s.length; rightIndex++) {
-    const item = s[rightIndex];
-
-    if (list.has(item)) {
-      res = Math.max(res, list.size);
-
-      while (s[leftIndex] !== item) {
-        list.delete(s[leftIndex]);
-        leftIndex += 1;
-      }
-
-      leftIndex += 1;
-    } else {
-      list.add(item);
+  let res = 0;
+  while (rightIndex < s.length) {
+    for (; rightIndex < s.length && !list.has(s[rightIndex]); rightIndex++) {
+      list.add(s[rightIndex]);
     }
-  }
-  res = Math.max(res, list.size);
+    res = Math.max(res, list.size);
 
+    if (rightIndex !== s.length) {
+      for (; s[leftIndex] !== s[rightIndex]; leftIndex++) {
+        list.delete(s[leftIndex]);
+      }
+      leftIndex += 1;
+    }
+    rightIndex += 1;
+  }
   return res;
 };
 
 // 1
-// console.log(lengthOfLongestSubstring('bbbb'));
+console.log(lengthOfLongestSubstring('bbbb'));
 
 // 3
 console.log(lengthOfLongestSubstring('pwwkew'));
