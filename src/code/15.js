06425f6400c4125041845c2fba6d3e23771a7377
Lenic <Lenic@live.cn>
2022-08-17 10:48:05 +0800

    更新 15 的比较后步进优化
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/15.js
+++ src/code/15.js
@@ -20,17 +20,18 @@ var threeSum = function (nums) {
       if (sum === 0) {
         res.push([nums[i], nums[left], nums[right]]);
 
-        while (left < right && nums[left] === nums[left + 1]) {
-          left += 1;
-        }
-        while (left < right && nums[right] === nums[right - 1]) {
-          right -= 1;
-        }
+        while (left < right && nums[left] === nums[left + 1]) left += 1;
+        while (left < right && nums[right] === nums[right - 1]) right -= 1;
+
         left += 1;
         right -= 1;
       } else if (sum < 0) {
+        while (left < right && nums[left] === nums[left + 1]) left += 1;
+
         left += 1;
       } else {
+        while (left < right && nums[right] === nums[right - 1]) right -= 1;
+
         right -= 1;
       }
     }
