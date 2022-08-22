d251964340d19d5d0aad5d29740121a2649dfd13
Lenic <Lenic@live.cn>
2022-08-22 14:30:08 +0800

    使用滑动窗口方法解决 930
    - 滑动窗口的解法只能用于这一种情况，不通用
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/930.js
+++ src/code/930.js
@@ -4,24 +4,29 @@
  * @return {number}
  */
 var numSubarraysWithSum = function (nums, goal) {
+  let right = 0;
+
   let res = 0;
-
-  let sums = new Array(nums.length + 1);
-  sums[0] = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    sums[i + 1] = sums[i] + nums[i];
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      const sum = sums[j + 1] - sums[i];
-      if (sum === goal) {
-        res += 1;
-      }
+  let sum1 = 0;
+  let left1 = 0;
+  let sum2 = 0;
+  let left2 = 0;
+  while (right < nums.length) {
+    sum1 += nums[right];
+    while (left1 <= right && sum1 > goal) {
+      sum1 -= nums[left1];
+      left1 += 1;
     }
-  }
 
+    sum2 += nums[right];
+    while (left2 <= right && sum2 >= goal) {
+      sum2 -= nums[left2];
+      left2 += 1;
+    }
+
+    right += 1;
+    res += left2 - left1;
+  }
   return res;
 };
 
