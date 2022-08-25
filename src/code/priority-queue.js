befa0ec735ddc7761896b0e7cc70478e74189f39
Lenic <Lenic@live.cn>
2022-08-25 12:10:56 +0800

    添加优先级队列帮助类
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ src/code/priority-queue.js
@@ -0,0 +1,69 @@
+import Heap from './heap';
+
+/**
+ * @template T
+ */
+class PriorityQueue {
+  /**
+   * @param {(x: T, y: T) => number} comparer
+   */
+  constructor(comparer) {
+    /**
+     * @type {T[]}
+     */
+    this.list = [];
+    this.heap = new Heap(this.list, comparer);
+
+    this.peek = this.peek.bind(this);
+    this.poll = this.poll.bind(this);
+    this.offer = this.offer.bind(this);
+  }
+
+  /**
+   * @returns {T | undefined}
+   */
+  isEmpty() {
+    return !this.list.length;
+  }
+
+  /**
+   * @returns {T | undefined}
+   */
+  peek() {
+    if (this.list.length) {
+      return this.list[0];
+    } else {
+      return undefined;
+    }
+  }
+
+  /**
+   * @param {T} val
+   * @returns {undefined}
+   */
+  offer(val) {
+    this.list.push(val);
+    this.heap.up(this.list.length - 1);
+  }
+
+  /**
+   * @returns {T | undefined}
+   */
+  poll() {
+    if (this.list.length) {
+      const res = this.list[0];
+
+      const last = this.list.pop();
+      if (this.list.length) {
+        this.list[0] = last;
+        this.heap.down(0);
+      }
+
+      return res;
+    } else {
+      return undefined;
+    }
+  }
+}
+
+export default PriorityQueue;
