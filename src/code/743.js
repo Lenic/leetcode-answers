2ef723324ab16b872df26777a24bdcc656e6131c
Lenic <Lenic@live.cn>
2022-08-25 19:29:12 +0800

    添加题目 743
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ src/code/743.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var networkDelayTime = function (times, n, k) {};
+
+// 2
+console.log(
+  networkDelayTime(
+    [
+      [2, 1, 1],
+      [2, 3, 1],
+      [3, 4, 1],
+    ],
+    4,
+    2
+  )
+);
