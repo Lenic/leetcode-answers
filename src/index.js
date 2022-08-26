9153423135d9bd4624cdd8470ddb49f40b1aa19c
Lenic <Lenic@live.cn>
2022-08-26 14:53:26 +0800

    初始化 TapTap 笔试题文件夹
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ const App = () => {
       originalLog.apply(console, args);
     };
 
-    import('./code');
+    // import('./code');
+    import('./taptap');
     // import('./brix');
     // import('./toptal');
     // import('./booking');
