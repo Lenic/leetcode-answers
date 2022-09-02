473f1284e28135c2d2493e4f614ac1d53524446f
Lenic <Lenic@live.cn>
2022-09-02 13:18:08 +0800

    使用自己实现的回溯算法完成题目：40
    - 剪枝条件有点儿不对劲
    
    Signed-off-by: Lenic <Lenic@live.cn>

---
+++ src/code/back-tracking/40.js
@@ -0,0 +1,60 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function (candidates, target) {
+  candidates.sort((x, y) => x - y);
+
+  /**
+   * @type {number[][]}
+   */
+  const res = [];
+  /**
+   * @param {number[]} ans
+   * @param {number} prefixSum
+   * @param {number} index
+   */
+  const traversal = (ans, prefixSum, index) => {
+    if (prefixSum === target) {
+      res.push([...ans]);
+      return;
+    }
+    if (index >= candidates.length) return;
+
+    for (let i = index; i < candidates.length; i++) {
+      if (i > 0 && !ans.length) {
+        if (candidates[i] === candidates[i - 1]) {
+          continue;
+        }
+      }
+
+      const nextSum = prefixSum + candidates[i];
+      if (nextSum > target) {
+        break;
+      }
+
+      ans.push(candidates[i]);
+      traversal(ans, nextSum, i + 1);
+      ans.pop();
+
+      while (i + 1 < candidates.length && candidates[i] === candidates[i + 1]) {
+        i += 1;
+      }
+    }
+  };
+
+  traversal([], 0, 0);
+  return res;
+};
+
+// [
+//   [1, 1, 6],
+//   [1, 2, 5],
+//   [1, 7],
+//   [2, 6],
+// ];
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8));
+
+// [[1, 2, 2], [5]];
+console.log(combinationSum2([2, 5, 2, 1, 2], 5));
