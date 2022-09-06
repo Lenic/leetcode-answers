2ae0d445f04a5d2b5fca97e3525ee566e16158e8
Lenic <Lenic@live.cn>
2022-09-06 21:46:18 +0800

    使用回溯算法解决题目：89
    - 纯回溯没技巧
    - 完全根据题目要求做出来的结果
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- src/code/back-tracking/89.ts
+++ src/code/back-tracking/89.ts
@@ -1,11 +1,55 @@
 function grayCode(n: number): number[] {
-  const max = 1 << n;
+  const max = (1 << n) - 1;
+  const ans: number[][] = [new Array(n).fill(0)];
+
+  const set = new Set<string>();
+  set.add(ans[0].join(''));
+
+  const traversal = (bytes: number[], previous: number, level: number): boolean => {
+    if (level === max) {
+      let count = 0;
+      for (let item of bytes) {
+        if (item === 1) {
+          count += 1;
+          if (count > 1) return false;
+        }
+      }
+      return count === 1;
+    }
+
+    const swap = (i: number) => (bytes[i] = bytes[i] === 1 ? 0 : 1);
+
+    for (let i = 0; i < n; i++) {
+      if (i === previous) continue;
+
+      swap(i);
+      ans.push(bytes.slice());
+
+      const str = bytes.join('');
+      if (set.has(str)) {
+        swap(i);
+        ans.pop();
+
+        continue;
+      }
+      set.add(str);
+
+      if (traversal(bytes, i, level + 1)) return true;
+
+      ans.pop();
+      set.delete(str);
+      bytes[i] = bytes[i] === 1 ? 0 : 1;
+    }
+
+    return false;
+  };
+
+  traversal(ans[0].slice(), -1, 0);
 
   const res: number[] = [];
-  for (let i = 0; i < max; i++) {
-    res.push((i >> 1) ^ i);
+  for (let item of ans) {
+    res.push(parseInt(item.join(''), 2));
   }
-
   return res;
 }
 
